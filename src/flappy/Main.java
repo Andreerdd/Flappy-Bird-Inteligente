f6f8807a72145366ee534d098609de351100cfec
dellagaspera <gabriel.gaspera@gmail.com>
2025-11-03 22:43:52 -0300

    JAVAAAAA

--- src/flappy/Main.java
+++ src/flappy/Main.java
@@ -3,6 +3,8 @@ package flappy;
 import processing.core.PApplet;
 
 public class Main {
+	public static FlappyBirdInteligente app;
+
 	public static void main(String[] args) {
 		PApplet.main("flappy.FlappyBirdInteligente");
 	}
